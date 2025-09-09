6712d7015fcf6e26f67a9d77ab4ada59b88081ea
Vatsal1118 <63930032+Vatsal1118@users.noreply.github.com>
2025-09-08 19:15:38 -0700

    added pedro tele correction driving and updated to nextftc v1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Dinks.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.extensions.pedro.PedroDriverControlled;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.hardware.driving.DriverControlledCommand;
+import dev.nextftc.hardware.driving.MecanumDriverControlled;
+import dev.nextftc.hardware.impl.MotorEx;
+
+@TeleOp
+public class Dinks extends dev.nextftc.ftc.NextFTCOpMode {
+    public Dinks() {
+        super();
+    }
+    public void onInit() {
+        addComponents(
+
+                new PedroComponent(Constants::createFollower)
+        );
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+        DriverControlledCommand driverControlled = new PedroDriverControlled(
+                Gamepads.gamepad1().leftStickY(),
+                Gamepads.gamepad1().leftStickX(),
+                Gamepads.gamepad1().rightStickX()
+        );
+        driverControlled.schedule();
+    }
+
+}
