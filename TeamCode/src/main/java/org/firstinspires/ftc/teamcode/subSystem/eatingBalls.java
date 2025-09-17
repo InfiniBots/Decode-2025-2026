269ea02e5eb47130ee8239a36050ae5a925ad944
coolaj <forajayg@gmail.com>
2025-09-16 21:36:41 -0700

    I fixed like a billion logic errors in the angle and velocity calculator and i prob will have fix more when tuning. I moved the color sensor to the turretGoPewPew file and added the scaling thing. The air sorter thing is ready but we still need to like combine in with the color sensor and april tag detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystem/eatingBalls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystem/eatingBalls.java
@@ -12,20 +12,11 @@ import java.lang.reflect.Array;
 
 public class eatingBalls {
     private DcMotorEx intake;
-    private RevColorSensorV3 colorSensor;
     public static double intakePow=0.8;
     public static double outtakePow=-0.8;
-    //color stuff
-    public static double fullThreshold=10;
-    public static double greenMin=100;
-    public static double greenMax=150;
-    public static double purpleMin=270;
-    public static double purpleMax=330;
-    public float[] hsv=new float[3];
-    public static String[] ballsInside={"x","x","x"};
+
     public eatingBalls(LinearOpMode op){
         intake = op.hardwareMap.get(DcMotorEx.class,"intake");
-        colorSensor = op.hardwareMap.get(RevColorSensorV3.class,"colorSensor");
     }
     //color sensor
     public void intaking(){
@@ -33,21 +24,5 @@ public class eatingBalls {
     }
     public void outtaking(){//idk when u would use this but maybe if u alr have 3 balls ig
         intake.setPower(outtakePow);
-    }
-    public boolean fullCheck(){
-        return colorSensor.getDistance(DistanceUnit.MM)<fullThreshold;
-    }
-    public String colorCheck(){
-        String ballColor = "unknown";
-        Color.RGBToHSV(colorSensor.red(), colorSensor.green(),  colorSensor.blue(),hsv);
-        if(hsv[0]<greenMax&&hsv[0]>greenMin){
-            ballColor="green";
-        }
-        else if(hsv[0]<purpleMax&&hsv[0]>purpleMin){
-            ballColor="green";
-        }else{
-            ballColor="unkown";
-        }
-        return ballColor;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystem/turretGoPewPew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystem/turretGoPewPew.java
@@ -19,15 +19,19 @@ public class turretGoPewPew  {
     private DcMotorEx turrMover;
     private DcMotorEx mainFlyWheel;
     private Servo shotAngler;
+    private RevColorSensorV3 colorSensor;
 
     //TODO tune like everything
     public boolean isRed;
-    public static double ygoal=138; //prob decently accurate, got from pedro visualizer
+    public static double ygoal=138*0.0254; //prob decently accurate, got from pedro visualizer
+    public static double goalHeight=1.1176; //in meters
+    public static double turrHeight=0.4318; //in meters
     public double goalx;
     public static double speedBoost=0;//extra power
     public static int ticksFor1Rotation=10000;
     public static double slope=1;
-
+    public static double delay=0.2;
+    public static double maxSpeed=22.619467;//in m/s of our flywheel motor assuming it has 72mm diameter and 6k rpm like sushi said
     //pid
     public double ErrorSum=0;
     public double lastError=0;
@@ -35,38 +39,60 @@ public class turretGoPewPew  {
     public double ki=0;
     public double kd=0;
     public long prevTime=0;
+    //color stuff
+    public static double fullThreshold=10;
+    public static double greenMin=100;
+    public static double greenMax=150;
+    public static double purpleMin=270;
+    public static double purpleMax=330;
+    public float[] hsv=new float[3];
 
     public turretGoPewPew(LinearOpMode op, boolean isRed){
         this.isRed=isRed;
+        goalx=isRed?136*0.0254:11*0.0254;
         turrMover = op.hardwareMap.get(DcMotorEx.class,"turrMover");
         mainFlyWheel = op.hardwareMap.get(DcMotorEx.class,"mainFlyWheel");
         shotAngler = op.hardwareMap.get(Servo.class,"shotAngler");
+        colorSensor = op.hardwareMap.get(RevColorSensorV3.class,"colorSensor");
 
 
         turrMover.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
-    //angler
-    private double idealAngle(double boty, double xdistance){
-         double ydelta=ygoal-boty;
-        double angOfElev=Math.atan(ydelta/xdistance);
-        double angle=(Math.toRadians(90)+angOfElev)/2;
+    //indexing calc
+    private double indexingTimeofFli(double boty, double botx) {
+        double boty_conv=boty*0.0254;
+        double botx_conv=botx*0.0254;
+        double botdeltax=goalx-botx_conv;
+        double botdeltay=ygoal-boty_conv;
+        double xdistance=Math.sqrt(botdeltay*botdeltay+botdeltax*botdeltax);
+        double angle = Math.toRadians(idealAngle(boty, botx));
+        double V = (idealpPow(boty, botx)-speedBoost)*maxSpeed;//convert pow back to vel
+        return (xdistance / (V * Math.cos(angle))+delay);
+    }
+    public double indexingAng(double boty, double botx) {
+        double boty_conv=boty*0.0254;//convert in to m for calc
+        double botx_conv=botx*0.0254;
+        double botdeltax=goalx-botx_conv;
+        double botdeltay=ygoal-boty_conv;
+        double xdistance=Math.sqrt(botdeltay*botdeltay+botdeltax*botdeltax);
+        double timeOfFlight=indexingTimeofFli(boty,botx);
+        double ydelta = goalHeight - turrHeight;
+        double vy = (ydelta + 0.5 * 9.8 * timeOfFlight * timeOfFlight) / timeOfFlight;
+        double vx = xdistance / timeOfFlight;
+        double angle = Math.atan(vy / vx);
         return Math.toDegrees(angle);
     }
-    public void angler(double boty, double xdistance){
-        double angle=idealAngle(boty,xdistance);
-        //TODO somehow convert angle to servo pos prob by graphing points os servos poses and their angle to get line of best fit
-        double pos=angle/slope;//slope is what i would get from graphing points for example it could be 30deg/0.1 pos and if my angle is 60 and
-        // divide by slope i get servos pos 0.2
-        shotAngler.setPosition(pos);
-    }
-    //launcher
-    private double idealpPow(double boty, double xdistance){
-        double angle=Math.toRadians(idealAngle(boty,xdistance));
-        double ydelta=ygoal-boty;
-        double Vyt=(Math.tan(angle))*(xdistance);
-        double xcomp=Math.sqrt(1/((ydelta-Vyt)/(0.5*(-9.8)*xdistance*xdistance)));
-        double V=xcomp/Math.cos(angle);
-        double maxSpeed=22.619467;//in m/s of our flywheel motor assuming it has 72mm diameter and 6k rpm like sushi said
+    private double indexingVel(double boty, double botx){
+        double boty_conv=boty*0.0254;//convert in to m for calc
+        double botx_conv=botx*0.0254;
+        double botdeltax=goalx-botx_conv;
+        double botdeltay=ygoal-boty_conv;
+        double xdistance=Math.sqrt(botdeltay*botdeltay+botdeltax*botdeltax);
+        double timeOfFlight=indexingTimeofFli(boty, botx);
+        double ydelta = goalHeight - turrHeight;
+        double vy = (ydelta + 0.5 * 9.8 * timeOfFlight * timeOfFlight) / timeOfFlight;
+        double vx = xdistance / timeOfFlight;
+        double V = Math.sqrt(vx*vx + vy*vy);
         double power=V/maxSpeed;
         power=power+speedBoost;
         if(V>=maxSpeed){
@@ -74,8 +100,48 @@ public class turretGoPewPew  {
         }
         return power;
     }
-    public void launchPow(double boty, double xdistance){
-        double pow=idealpPow(boty,xdistance);
+    //angler
+
+    private double idealAngle(double boty, double botx){
+        double boty_conv=boty*0.0254;//convert in to m for calc
+        double botx_conv=botx*0.0254;
+        double botdeltax=goalx-botx_conv;
+        double botdeltay=ygoal-boty_conv;
+        double xdistance=Math.sqrt(botdeltay*botdeltay+botdeltax*botdeltax);
+        double ydelta=goalHeight-turrHeight;
+        double angOfElev=Math.atan(ydelta/xdistance);
+        double angle=(Math.toRadians(90)+angOfElev)/2;
+        return Math.toDegrees(angle);
+    }
+
+    public void angler(double boty, double botx, boolean isIndexing){
+        double angle=isIndexing?indexingAng(boty,botx):idealAngle(boty,botx);
+        //TODO somehow convert angle to servo pos prob by graphing points os servos poses and their angle to get line of best fit
+        double pos=angle/slope;//slope is what i would get from graphing points for example it could be 30deg/0.1 pos and if my angle is 60 and
+        // divide by slope i get servos pos 0.2
+        shotAngler.setPosition(pos);
+    }
+    //launcher
+    private double idealpPow(double boty, double botx){
+        double boty_conv=boty*0.0254;//convert in to m for calc
+        double botx_conv=botx*0.0254;
+        double botdeltax=goalx-botx_conv;
+        double botdeltay=ygoal-boty_conv;
+        double xdistance=Math.sqrt(botdeltay*botdeltay+botdeltax*botdeltax);
+        double angle=Math.toRadians(idealAngle(boty,botx));
+        double ydelta=goalHeight-turrHeight;
+        double Vyt=(Math.tan(angle))*(xdistance);
+        double xcomp=Math.sqrt(1/((ydelta-Vyt)/(0.5*(-9.8)*xdistance*xdistance)));
+        double V=xcomp/Math.cos(angle);
+        double power=V/maxSpeed;
+        power=power+speedBoost;
+        if(V>=maxSpeed){
+            power=1;
+        }
+        return power;
+    }
+    public void launchPow(double boty, double botx, boolean isIndexing){
+        double pow=isIndexing?indexingVel(boty,botx):idealpPow(boty,botx);
         mainFlyWheel.setPower(pow);
     }
     //aimer
@@ -90,7 +156,6 @@ public class turretGoPewPew  {
         return ((kp*Error)+(ki*ErrorSum)+(kd*errorChange));
     }
     private long targetpos(double botx, double boty, double botTheta){
-        goalx=isRed?136:11;
         double ticksPerAng=(ticksFor1Rotation/360);
         double xLength=(goalx-botx);
         double yLength=(ygoal-boty);
@@ -108,4 +173,33 @@ public class turretGoPewPew  {
         turrMover.setPower(pow);
     }
 
+    //color stuff
+    public boolean fullCheck(){
+        return colorSensor.getDistance(DistanceUnit.MM)<fullThreshold;
+    }
+    public String colorCheck(){
+        String ballColor = "unknown";
+        double raw_red = colorSensor.red();
+        double raw_green = colorSensor.green();
+        double raw_blue = colorSensor.blue();
+        double max = Math.max(Math.max(raw_red, raw_green), raw_blue);
+        int red=0;
+        int green=0;
+        int blue=0;
+        if (max > 0) {
+            red=(int)((raw_red/max)*255);
+            green=(int)((raw_green/max)*255);
+            blue=(int)((raw_blue/max)*255);
+        }
+        Color.RGBToHSV(red, green, blue,hsv);
+        if(hsv[0]<greenMax&&hsv[0]>greenMin){
+            ballColor="green";
+        }
+        else if(hsv[0]<purpleMax&&hsv[0]>purpleMin){
+            ballColor="purple";
+        }else{
+            ballColor="unknown";
+        }
+        return ballColor;
+    }
 }
