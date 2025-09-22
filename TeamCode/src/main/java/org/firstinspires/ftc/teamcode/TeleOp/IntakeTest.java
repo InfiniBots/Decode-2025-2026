860c38cb4ff5b5a2f2c9b8f0fbbd79c02512b57e
Vatsal1118 <63930032+Vatsal1118@users.noreply.github.com>
2025-09-21 22:48:09 -0700

    added velocity logger to intaketest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/IntakeTest.java
@@ -6,6 +6,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Config
 @TeleOp
@@ -22,12 +26,30 @@ public class IntakeTest extends LinearOpMode {
         Motor1 = hardwareMap.get(DcMotorEx.class, "Motor1");
         Motor2 = hardwareMap.get(DcMotorEx.class, "Motor2");
 
+        //List<Double> timeStamps = new ArrayList<>();
+        //List<Double> velocities = new ArrayList<>();
+
+        ElapsedTime timer = new ElapsedTime();
+        double lastTime = 0;
+        timer.reset();
+
         waitForStart();
 
         while (opModeIsActive()) {
             Motor0.setPower(1);
             Motor1.setPower(1);
-            Motor2.setPower(1);}
+            Motor2.setPower(1);
+
+            double now = timer.seconds();
+            double velocity = Motor2.getVelocity();
+
+            if (now-lastTime >= 0.5) {
+                telemetry.addData("Time", "%.2f", now);
+                telemetry.addData("Velocity", "%.2f", velocity);
+                telemetry.update();
+                lastTime = now;
+            }
         }
     }
+}
 
