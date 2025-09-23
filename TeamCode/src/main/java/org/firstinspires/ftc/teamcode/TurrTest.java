ead3af486e3ce3ba6ae0c1464df686cedb54923d
coolaj <forajayg@gmail.com>
2025-09-22 21:14:32 -0700

    turrtester but with velocity telemtry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurrTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurrTest.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
@@ -19,13 +21,14 @@ public class TurrTest extends LinearOpMode {
     private DcMotorEx intakeStage2;
     public static double pow=1;
     public static double vel=0.5;
-    public static boolean powering=true;
+    public static boolean powering=false;
     public static double tickPerRev=29;
     public static int RPM=1150;
     public static double intakePow=0.8;
 
     @Override
     public void runOpMode() {
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
         TurrMotor = hardwareMap.get(DcMotorEx.class,"Motor0");
         intakeStage1 = hardwareMap.get(DcMotorEx.class,"Motor1");
         intakeStage2= hardwareMap.get(DcMotorEx.class,"Motor2");
@@ -47,6 +50,8 @@ public class TurrTest extends LinearOpMode {
                     TurrMotor.setVelocity(0);
                 }
             }
+            telemetry.addData("current velocity",TurrMotor.getVelocity());
+            telemetry.update();
         }
     }
 }
