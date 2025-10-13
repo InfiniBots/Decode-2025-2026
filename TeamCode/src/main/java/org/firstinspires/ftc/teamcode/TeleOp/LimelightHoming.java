0d946ab2e4ca203642352d5e9ff15e0a120edf58
Toadywo <nsmbwtoady@gmail.com>
2025-10-12 17:55:59 -0700

    new code that homes on the april tag, ki kp kd all require tuning

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LimelightHoming.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import java.util.List;
+
+@TeleOp
+public class LimelightHoming extends LinearOpMode {
+
+    private DcMotorEx turretMotor;
+    private Limelight3A limelight;
+
+    private final double kP = 0.0; // tune
+    private final double kI = 0.0; // tune
+    private final double kD = 0.0; // tune
+
+    private double integral = 0;
+    private double lastError = 0;
+
+    public void runOpMode() {
+        turretMotor = hardwareMap.get(DcMotorEx.class, "turretMotor");
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+            List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
+            if (fiducials.isEmpty()) {
+                LLResultTypes.FiducialResult fiducial = fiducials.get(0);
+
+                double x = fiducial.getTargetXDegrees();
+                double y = fiducial.getTargetYDegrees();
+
+                double error = (x + y) / 2;
+
+                integral += error;
+                double derivative = error - lastError;
+                lastError = error;
+
+                double newPower = kP * error + kI * integral + kD * derivative;
+
+                turretMotor.setPower(newPower);
+
+                telemetry.addData("Tracking ID: ", fiducial);
+                telemetry.addData("Target X: ", x);
+                telemetry.addData("Target Y: ", y);
+                telemetry.addData("Motor Power: ", newPower);
+                telemetry.update();
+
+
+            }
+        }
+
+    }
+}
