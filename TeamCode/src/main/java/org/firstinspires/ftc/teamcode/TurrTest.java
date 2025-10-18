f881809cf0f3338390c3882dd281450d0130a122
coolaj <forajayg@gmail.com>
2025-10-17 21:48:57 -0700

    Cleaned up TurrTest and added TurrTestv2 to keep our own pidf (yes i added another constant thingy idk if i did it right tho) I also think i found a way to adjust velocity in a much easier way that all that physcis stuff we were doing before

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurrTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurrTest.java
@@ -26,30 +26,11 @@ public class TurrTest extends LinearOpMode {
     private DcMotorEx intakeStage1;
     private DcMotorEx intakeStage2;
     private DcMotorEx TurrMotor2;
+    public static double powerinpow=1;
     public static double pow=1;
     public static boolean powering=false;
-    public static double velocity_MperS=2;//ignore this for now
+    public static int ticksPSec;
     public static double intakePow=1;
-    public static int ticksPerSecond=700;
-    private double velocity;
-    //pid
-    public static boolean pid=false;
-    private double lastError=0;
-    private double ErrorSum=0;
-    private long lastTime=0;
-    public static double kp=0;
-    public static double kd=0;
-    public static double ki=0;
-    private double PID(double initPos,double targetPos,long time){
-        double Error = targetPos-initPos;
-        if(time<=0){
-            time=1;
-        }
-        double errorChange=(Error-lastError)/time;
-        ErrorSum+=(Error*time);
-        lastError=Error;
-        return ((kp*Error)+(ki*ErrorSum)+(kd*errorChange));
-    }
 
     @Override
     public void runOpMode() {
@@ -58,13 +39,11 @@ public class TurrTest extends LinearOpMode {
         TurrMotor = hardwareMap.get(DcMotorEx.class,"Motor0");
         //intakeStage1 = hardwareMap.get(DcMotorEx.class,"Motor0");
         //intakeStage2= hardwareMap.get(DcMotorEx.class,"Motor1");
-        TurrMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);//gets encoder stuff
-        TurrMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);//gets encoder stuff
+        TurrMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        TurrMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         TurrMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         waitForStart();
-        lastTime=System.currentTimeMillis();
         while (opModeIsActive()) {
-            long curTime = System.currentTimeMillis();
            /* if(gamepad1.b){
                 intakeStage1.setPower(intakePow);//sets intake power
                 intakeStage2.setPower(intakePow);//sets intake power
@@ -75,49 +54,25 @@ public class TurrTest extends LinearOpMode {
 
             if(powering) {//when u want to use power instead of velocity
                 if (gamepad1.a) {
-                    TurrMotor.setPower(pow);//set power when press a
-                   // TurrMotor2.setPower(pow);
+                    TurrMotor.setPower(powerinpow);
+                    TurrMotor2.setPower(powerinpow);
                 } else {
                     TurrMotor.setPower(0);// sets to 0 when not pressed
-                   // TurrMotor2.setPower(0);
+                    TurrMotor2.setPower(0);
                 }
             }else if(!powering){
                 if (gamepad1.a) { //castrate gautam
-                    telemetry.addData("in",1);
-                    if (pid){
-                        TurrMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                        TurrMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);//gets encoder stuff
-                        long deltaTime=curTime-lastTime;
-                        lastTime=curTime;
-                        double pow=PID(TurrMotor.getVelocity(),ticksPerSecond,deltaTime);
-                        pow=(pow+1)/2;
-                        TurrMotor.setPower(-pow);
-                        TurrMotor2.setPower(-pow);
-                    }else {
-                        TurrMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                        TurrMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-                        TurrMotor.setVelocity(ticksPerSecond);
-                        TurrMotor2.setVelocity(ticksPerSecond);
-                        //  TurrMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);//gets encoder stuff
-
-                      //  TurrMotor2.setVelocity(ticksPerSecond);
-                    }
+                    TurrMotor.setVelocity(ticksPSec);
+                    TurrMotor2.setVelocity(ticksPSec);
                 } else {
                     TurrMotor.setVelocity(0);// sets to 0 when not pressed
                     TurrMotor2.setVelocity(0);// sets to 0 when not pressed
-                    telemetry.addData("out",0);
-                   // TurrMotor2.setVelocity(0);
-                  //  TurrMotor.setPower(0);// sets to 0 when not pressed
-                   // TurrMotor2.setPower(0);
                 }
             }
             //telemtry
             telemetry.addData("TurrMotor velocity",TurrMotor.getVelocity());
-            telemetry.addData("TurrMotor currTicks: ", TurrMotor.getCurrentPosition());
-            telemetry.addData("TurrMotor 2 currTicks: ", TurrMotor2.getCurrentPosition());
-            telemetry.addData("TurrMotor 2 velocity", TurrMotor2.getVelocity());
-            telemetry.addData("Target speed",ticksPerSecond);
+            telemetry.addData("TurrMotor2 velocity", TurrMotor2.getVelocity());
+            telemetry.addData("Target speed",ticksPSec);
             telemetry.addData("Voltage",TurrMotor.getCurrent(CurrentUnit.AMPS)+TurrMotor2.getCurrent(CurrentUnit.AMPS));
             telemetry.addData("power",TurrMotor.getPower());
             PanelsTelemetry.INSTANCE.getTelemetry().addData("hi",1);
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurrTestv2.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.util.InterpLUT;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+@TeleOp
+@Config
+public class TurrTestv2 extends LinearOpMode {
+    private DcMotorEx TurrMotor;
+    private DcMotorEx TurrMotor2;
+    private InterpLUT graph;
+
+    public static int ticksPerSecond = 700;
+    private long currTime;
+    private long deltaTime;
+
+    //PID stuff
+    public static double kp = 0.0;
+    public static double ki = 0.0;
+    public static double kd = 0.0;
+    public static double kf = 0.0;
+    private double lastError = 0;
+    private double errorSum = 0;
+    private long lastTime = 0;
+
+    private double PID(double currentVelocity, double targetVelocity, long time) {
+        double error = targetVelocity - currentVelocity;
+        if (time <= 0) {
+            time = 1;
+        }
+        double errorChange = (error - lastError) / time;
+        errorSum += (error * time);
+        lastError = error;
+        return ((kp * error) + (ki * errorSum) + (kd * errorChange) + (kf * targetVelocity));//added new velocity thingy
+    }
+    void getVel(){// we doing in ft and tickspersecond
+        graph.add(1,1);//need to add points (ts is random right now)
+        graph.createLUT();
+    }
+
+    @Override
+    public void runOpMode() {
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+        TurrMotor = hardwareMap.get(DcMotorEx.class, "Motor0");
+        TurrMotor2 = hardwareMap.get(DcMotorEx.class, "Motor1");
+
+        TurrMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        TurrMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        TurrMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        lastTime = System.currentTimeMillis();
+
+        while (opModeIsActive()) {
+            currTime = System.currentTimeMillis();
+            if (gamepad1.a) {
+                deltaTime = currTime - lastTime;
+                double power = PID(TurrMotor.getVelocity(), ticksPerSecond, deltaTime);
+                lastTime = currTime;
+                TurrMotor.setPower(power);
+                TurrMotor2.setPower(power);
+            } else {
+                TurrMotor.setPower(0);
+                TurrMotor2.setPower(0);
+                lastTime = currTime;
+            }
+
+            telemetry.addData("TurrMotor Velocity", TurrMotor.getVelocity());
+            telemetry.addData("TurrMotor2 Velocity", TurrMotor2.getVelocity());
+            telemetry.addData("Target Speed", ticksPerSecond);
+            telemetry.addData("Error", ticksPerSecond - TurrMotor.getVelocity());
+            telemetry.addData("Power", TurrMotor.getPower());
+            telemetry.addData("Current", TurrMotor.getCurrent(CurrentUnit.AMPS) + TurrMotor2.getCurrent(CurrentUnit.AMPS));
+            telemetry.update();
+        }
+    }
+}
