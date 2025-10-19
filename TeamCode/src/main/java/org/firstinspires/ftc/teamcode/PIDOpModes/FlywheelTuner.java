06721a6afc3a0a160f2282b877051992d97bfae3
Vatsal1118 <63930032+Vatsal1118@users.noreply.github.com>
2025-10-18 23:35:34 -0700

    i think i fixed my velocity pid, but its prbably useless now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDOpModes/FlywheelTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDOpModes/FlywheelTuner.java
@@ -1,14 +1,19 @@
 package org.firstinspires.ftc.teamcode.PIDOpModes;
 
-import com.acmerobotics.dashboard.config.Config;
-import com.bylazar.configurables.annotations.Configurable;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.subSystem.FlywheelVelocityPID;
-import dev.nextftc.bindings.Button;
-import dev.nextftc.ftc.NextFTCOpMode;
-import static dev.nextftc.bindings.Bindings.*;
+import static dev.nextftc.bindings.Bindings.button;
 
-@Configurable
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subSystem.FlywheelVelocityPID;
+
+import dev.nextftc.bindings.Bindings;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+
 @Config
 @TeleOp
 public class FlywheelTuner extends NextFTCOpMode {
@@ -18,14 +23,17 @@ public class FlywheelTuner extends NextFTCOpMode {
     @Override
     public void onInit() {
         flywheel = new FlywheelVelocityPID(hardwareMap, telemetry);
+        addComponents(
+                new SubsystemComponent(flywheel),
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
+        );
     }
 
     @Override
     public void onStartButtonPressed() {
-        button(() -> gamepad1.a)
-                .whenBecomesTrue(() -> flywheel.runToRPM(targetRPM));
-        button(() -> gamepad1.b)
-                .whenBecomesTrue(() -> flywheel.stop());
+        button(() -> gamepad1.a).whenBecomesTrue(flywheel.runToRPM(targetRPM));
+        button(() -> gamepad1.b).whenBecomesTrue(flywheel.runToRPM(targetRPM));
     }
 
     @Override
